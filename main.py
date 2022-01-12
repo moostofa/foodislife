9bfff6943928d60d6c77a535b57ccfab10a960dd
moostofa <mmdrahinz96@gmail.com>
2022-01-13 00:23:23 +1100

    display restaurant names and descriptions

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ r.html.render(sleep=2, keep_page=True, scrolldown=1)
 restaurants = r.html.find(".venue-list-item")
 
 for venue in restaurants:
-    title = venue.find(".listing-title")[0]
-    print(title.text)
+    title = venue.find(".listing-title")[0].text
+    description = venue.find(".listing-description")[0].text
+    print(f"||| {title} |||")
+    print(description)
+    print("----------")
 
 #title = restaurants[0].find(".listing-title")
 #print(title[0].text)
