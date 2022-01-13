bb249893daeb5bb7d5f260c33ba8ab0045b0f634
moostofa <mmdrahinz96@gmail.com>
2022-01-13 12:40:26 +1100

    able to scrape cusine and location details for restuarants

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from requests_html import HTMLSession
+import re
 
 url = "https://www.happycow.net/oceania/australia/new_south_wales/sydney/"
 
@@ -10,9 +11,17 @@ restaurants = r.html.find(".venue-list-item")
 for venue in restaurants:
     title = venue.find(".listing-title")[0].text
     description = venue.find(".listing-description")[0].text
-    print(f"||| {title} |||")
-    print(description)
+    cuisine = venue.find("p", containing="Cuisine:")[0].text
+    location = venue.find("p", containing="Sydney, New South Wales")[0].text
+
+    print(f"TITLE: {title}")
     print("----------")
+    print(f"DESCRIPTION: {description}")
+    print("----------")
+    print(f"LOCATION: {location}")
+    print("----------")
+    print(f"CUISINE: {cuisine}")
+    print("||||||||||")
 
 #title = restaurants[0].find(".listing-title")
 #print(title[0].text)
