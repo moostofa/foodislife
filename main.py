db207636393a74f447044b63036ff9f5ea13b385
moostofa <mmdrahinz96@gmail.com>
2022-01-13 14:30:41 +1100

    successfully retrieve restuarants fields iteratively

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 from requests_html import HTMLSession
-import re
 
 url = "https://www.happycow.net/oceania/australia/new_south_wales/sydney/"
 
 session = HTMLSession()
-r = session.get(url)
-r.html.render(sleep=2, keep_page=True, scrolldown=1)
-restaurants = r.html.find(".venue-list-item")
+response = session.get(url)
+response.html.render(sleep=1, keep_page=True, scrolldown=1)
+venues = response.html.find(".venue-list-item")
 
-for venue in restaurants:
-    title = venue.find(".listing-title")[0].text
-    description = venue.find(".listing-description")[0].text
-    cuisine = venue.find("p", containing="Cuisine:")[0].text
-    location = venue.find("p", containing="Sydney, New South Wales")[0].text
+fields = {
+    "title": {"selector": ".listing-title"},
+    "description": {"selector": ".listing-description"},
+    "location": {"containing": "Sydney, New South Wales"},
+    "cuisine": {"containing": "Cuisine:"},
+    "phone": {"containing": "+61-"}
+}
 
-    print(f"TITLE: {title}")
-    print("----------")
-    print(f"DESCRIPTION: {description}")
-    print("----------")
-    print(f"LOCATION: {location}")
-    print("----------")
-    print(f"CUISINE: {cuisine}")
-    print("||||||||||")
-
-#title = restaurants[0].find(".listing-title")
-#print(title[0].text)
+for venue in venues:
+    details = {}
+    for key, filters in fields.items():
+        try:
+            details[key] = venue.find(**filters)[0].text
+        except IndexError:
+            details[key] = ""
+    print(details)
