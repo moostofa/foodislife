cffb946a7231a3f085cd2922e56aa62602e2055f
moostofa <mmdrahinz96@gmail.com>
2022-01-13 18:15:48 +1100

    tested db connection + wrapped main.py code in a function

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from requests_html import HTMLSession
 
-url = "https://www.happycow.net/oceania/australia/new_south_wales/sydney/"
+# scrapes a webpage and returns a list of resturants & their details
+def get():
+    # url to scrape data from
+    url = "https://www.happycow.net/oceania/australia/new_south_wales/sydney/"
 
-session = HTMLSession()
-response = session.get(url)
-response.html.render(sleep=1, keep_page=True, scrolldown=1)
-venues = response.html.find(".venue-list-item")
+    # create html session to visit webpage
+    session = HTMLSession()
+    response = session.get(url)
+    response.html.render(sleep=1, keep_page=True, scrolldown=1)
 
-fields = {
-    "title": {"selector": ".listing-title"},
-    "description": {"selector": ".listing-description"},
-    "location": {"containing": "Sydney, New South Wales"},
-    "cuisine": {"containing": "Cuisine:"},
-    "phone": {"containing": "+61-"}
-}
+    # the fields of interest that will be saved to the DB, and the selectors/filters that will be used to find them
+    fields = {
+        "title": {"selector": ".listing-title"},
+        "description": {"selector": ".listing-description"},
+        "location": {"containing": "Sydney, New South Wales"},
+        "cuisine": {"containing": "Cuisine:"},
+        "phone": {"containing": "+61-"}
+    }
 
-for venue in venues:
-    details = {}
-    for key, filters in fields.items():
-        try:
-            details[key] = venue.find(**filters)[0].text
-        except IndexError:
-            details[key] = ""
-    print(details)
+    # iterate over all the venues & extract the required fields
+    venues = response.html.find(".venue-list-item")
+    for venue in venues:
+        details = {}
+        for key, filters in fields.items():
+            try:
+                details[key] = venue.find(**filters)[0].text
+            except IndexError:
+                details[key] = ""
+        print(details)
--- test.py
+++ test.py
@@ -1,42 +1,23 @@
-import time
+import mysql.connector
+from mysql.connector import Error
 
-import requests
-from bs4 import BeautifulSoup
-from requests_html import HTMLSession
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
 
-url = "https://www.happycow.net/oceania/australia/new_south_wales/sydney/"
+def connect():
+    """ Connect to MySQL database """
+    conn = None
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='foodme',
+                                       user='root',
+                                       password='#moth3rFuzzer')
+        if conn.is_connected():
+            print('Connected to MySQL database')
 
-# all methods below yield the same output - cannot access nodes any deeper than ".city-results"
+    except Error as e:
+        print(e)
 
-# using selenium webdriver
-def selenium_webdriver():
-    service = Service(executable_path=ChromeDriverManager().install())
-    browser = webdriver.Chrome(service=service)
-    browser.get(url)
-    restuarants = browser.find_element(By.CLASS_NAME, "city-results")
-    print(restuarants.text)
-    browser.quit()
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
 
-# using requests & beautifulsoup4
-def beautifulsoup4():
-    html = requests.get(url).text
-    time.sleep(10)
-    soup = BeautifulSoup(html, "html.parser")
-    restuarants = soup.find(class_="venue-city-items")  # this fails - returns NoneType
-    print(restuarants.prettify())
-
-# using requests-html (supposedly has support for JavaScript)
-def requests_html():
-    session = HTMLSession()
-    r = session.get(url)
-    r.html.render(sleep=1, keep_page=True, scrolldown=1)
-    restuarants = r.html.find(".venue-list-item")
-    print(restuarants)
-
-# beautifulsoup4()
-# selenium_webdriver()
-requests_html()
+connect()
